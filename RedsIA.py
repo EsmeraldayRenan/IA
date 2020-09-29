0c2163f57b6158136a5582c4355cc4e48f1b9e79
unknown <esmeronquillo@gmail.com>
2020-09-28 19:12:24 -0500

    Red semantica clasificación de animales

--- RedsIA.py
+++ RedsIA.py
@@ -9,99 +9,96 @@ MARTINEZ RONQUILLO ESMERALDA
 """
 
 conocimientoes = [
-        ("Gallo","Es","Vertebrado"),
-        ("Gallo","Es","Oviparo"),
-        ("Ballena","Es","Mammalia"),
-        ("Ballena","Es","Vivipara"),
-        ("Oso","Es","Mammalia"),
-        ("Oso","Es","Viviparo"),
-        ("Tortuga","Es","Sauropsido"),
-        ("Tortuga","Es","Oviparo"),
-        ("Cocodrilo","Es","Sauropsido"),
-        ("Cocodrilo","Es","Oviparo"),
-        ("Iguana","Es","Sauropsido"),
-        ("Iguana","Es","Oviparo"),
-        ("Gato","Es","Mammalia"),
-        ("Gato","Es","Viviparo"),
-        ("Delfin","Es","Mammalia"),
-        ("Delfin","Es","Viviparo"),
-        ("Sauropsido","Es","Mammalia"),
-        ("Sauropsido","Es","Tetrapodo"),
-        ("Mammalia","Es","Tetrapodo"),
-        ("Mammalia","Es","Vertebrado"),
-        ("Vertebrado","Es","Tetrapodo"),
-        ("Vertebrado","Es","Sauropsido"),
+        ("Gallo","Vertebrado"),
+        ("Gallo","Oviparo"),
+        ("Ballena","Mammalia"),
+        ("Ballena","Vivipara"),
+        ("Oso","Mammalia"),
+        ("Oso","Viviparo"),
+        ("Tortuga","Sauropsido"),
+        ("Tortuga","Oviparo"),
+        ("Cocodrilo","Sauropsido"),
+        ("Cocodrilo","Oviparo"),
+        ("Iguana","Sauropsido"),
+        ("Iguana","Oviparo"),
+        ("Gato","Mammalia"),
+        ("Gato","Viviparo"),
+        ("Delfin","Mammalia"),
+        ("Delfin","Viviparo"),
+        ("Sauropsido","Mammalia"),
+        ("Sauropsido","Tetrapodo"),
+        ("Mammalia","Tetrapodo"),
+        ("Mammalia","Vertebrado"),
+        ("Vertebrado","Tetrapodo"),
+        ("Vertebrado","Sauropsido"),
 ]
 
 conocimientotiene = [
-        ("Gallo","Tiene","Plumas"),
-        ("Ballena","Tiene","Piel"),
-        ("Oso","Tiene","Garras"),
-        ("Oso","Tiene","Pelo"),
-        ("Tortuga","Tiene","Garras"),
-        ("Tortuga","Tiene","Escamas"),
-        ("Cocodrilo","Tiene","Garras"),
-        ("Cocodrilo","Tiene","Escamas"),
-        ("Iguana","Tiene","Garras"),
-        ("Iguana","Tiene","Escamas"),
-        ("Gato","Tiene","Garras"),
-        ("Gato","Tiene","Pelo"),
-        ("Delfin","Tiene","Piel"),
+        ("Gallo","Plumas"),
+        ("Ballena","Piel"),
+        ("Oso","Garras"),
+        ("Oso","Pelo"),
+        ("Tortuga","Garras"),
+        ("Tortuga","Escamas"),
+        ("Cocodrilo","Garras"),
+        ("Cocodrilo","Escamas"),
+        ("Iguana","Garras"),
+        ("Iguana","Escamas"),
+        ("Gato","Garras"),
+        ("Gato","Pelo"),
+        ("Delfin","Piel"),
 ]
 
 conocimientovive = [
-        ("Gallo","Vive","Tierra"),
-        ("Ballena","Vive","Agua"),
-        ("Oso","Vive","Tierra"),
-        ("Tortuga","Vive","Agua"),
-        ("Tortuga","Vive","Tierra"),
-        ("Cocodrilo","Vive","Agua"),
-        ("Cocodrilo","Vive","Tierra"),
-        ("Iguana","Vive","Tierra"),
-        ("Gato","Vive","Tierra"),
-        ("Delfin","Vive","Agua"),
+        ("Gallo","Tierra"),
+        ("Ballena","Agua"),
+        ("Oso","Tierra"),
+        ("Tortuga","Agua"),
+        ("Tortuga","Tierra"),
+        ("Cocodrilo","Agua"),
+        ("Cocodrilo","Tierra"),
+        ("Iguana","Tierra"),
+        ("Gato","Tierra"),
+        ("Delfin","Agua"),
 ]
 
-def es(A,B,C,CON1):
+def es(A,B,CON1):
     r=0
     l=len(CON1)
     while r!=l :
         if CON1[r][0] == A:
             if CON1[r][1] == B:
-                if CON1[r][2] ==C:
-                    return True
+                return True
         r+=1
     else:
         return False
     
-def tiene(A,B,C,CON2):
+def tiene(A,B,CON2):
     r=0
     l=len(CON2)
     while r!=l :
         if CON2[r][0] == A:
             if CON2[r][1] == B:
-                if CON2[r][2] ==C:
-                    return True
+                return True
         r+=1
     else:
         return False
     
-def vive(A,B,C,CON3):
+def vive(A,B,CON3):
     r=0
     l=len(CON3)
     while r!=l :
         if CON3[r][0] == A:
             if CON3[r][1] == B:
-                if CON3[r][2] ==C:
-                    return True
+                return True
         r+=1
     else:
         return False
 
-print(es("Cocodrilo","Es","Sauropsido",conocimientoes))
-print(es("Tortuga","Es","Mammalia",conocimientoes))
-print(tiene("Ballena","Tiene","Piel",conocimientotiene))
-print(tiene("Iguana","Tiene","Piel",conocimientotiene))
-print(vive("Tortuga","Vive","Agua",conocimientovive))
-print(vive("Delfin","Vive","Tiera",conocimientovive))
+print(es("Cocodrilo","Sauropsido",conocimientoes))
+print(es("Tortuga","Mammalia",conocimientoes))
+print(tiene("Ballena","Piel",conocimientotiene))
+print(tiene("Iguana","Piel",conocimientotiene))
+print(vive("Tortuga","Agua",conocimientovive))
+print(vive("Delfin","Tiera",conocimientovive))
 
